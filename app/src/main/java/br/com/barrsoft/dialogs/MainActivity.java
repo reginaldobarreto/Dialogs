4bc7482968daa4a43624cfa4d7651e552710e6cf
Reginaldo Barreto <reginaldo.barreto@gmail.com>
2019-10-24 08:24:14 -0300

    adicionado alert multiplechoice checkbox

--- app/src/main/java/br/com/barrsoft/dialogs/MainActivity.java
+++ app/src/main/java/br/com/barrsoft/dialogs/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,13 +19,18 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
 
 
+    private static final String TAG = MainActivity.class.getName();
     @BindView(R.id.button)
     Button button;
     @BindView(R.id.button1)
     Button button1;
+    @BindView(R.id.button2)
+    Button button2;
     private AlertDialog alertDialog;
     private AlertDialog alertDialog1;
+    private AlertDialog alertDialog2;
     private String [] opcoes = {"Item 1","Item 2","Item 3"};
+    private Boolean [] selecionado = new Boolean[opcoes.length];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
 
 
         builder.setTitle(getString(R.string.alert_title));
-        builder.setIcon(R.drawable.ic_stat_alert);
         builder.setMessage(getString(R.string.alert_msg));
         builder.setPositiveButton(getString(R.string.alert_button_ok), new DialogInterface.OnClickListener() {
             @Override
@@ -54,21 +61,38 @@ public class MainActivity extends AppCompatActivity {
         alertDialog = builder.create();
 
 
-        AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
 
-        builder2.setIcon(R.drawable.ic_action_name);
-        builder2.setTitle(R.string.alert_title);
-        builder2.setSingleChoiceItems(opcoes, -1, new DialogInterface.OnClickListener() {
+        builder1.setTitle(R.string.alert_title_checkbox);
+        builder1.setSingleChoiceItems(opcoes, -1, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Toast.makeText(getApplicationContext()," " + opcoes[which],Toast.LENGTH_LONG).show();
             }
         });
-        builder2.setPositiveButton(getString(R.string.alert_button_ok),null);
+        builder1.setPositiveButton(getString(R.string.alert_button_ok),null);
+        builder1.setNegativeButton(getString(R.string.alert_button_cancel),null);
+        alertDialog1 = builder1.create();
+
+
+        AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
+
+        builder2.setTitle(R.string.alert_title_radio);
+        builder2.setMultiChoiceItems(opcoes, null, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                if (isChecked){
+                    selecionado[which]=isChecked;
+                    Toast.makeText(getApplicationContext(),getText(R.string.item).toString() + (which+1) + " " + selecionado[which].toString(),Toast.LENGTH_SHORT).show();
+                }else {
+                    selecionado[which]=isChecked;
+                    Toast.makeText(getApplicationContext(),getText(R.string.item).toString() + (which+1) + " "+ selecionado[which].toString(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        builder2.setPositiveButton(getString(R.string.alert_button_ok), null);
         builder2.setNegativeButton(getString(R.string.alert_button_cancel),null);
-        alertDialog1 = builder2.create();
-
-
+        alertDialog2 = builder2.create();
     }
 
     @OnClick(R.id.button)
@@ -81,5 +105,11 @@ public class MainActivity extends AppCompatActivity {
     public void submit1(View view) {
         // TODO submit data to server...
         alertDialog1.show();
+    }
+
+    @OnClick(R.id.button2)
+    public void submit2(View view) {
+        // TODO submit data to server...
+        alertDialog2.show();
     }
 }
